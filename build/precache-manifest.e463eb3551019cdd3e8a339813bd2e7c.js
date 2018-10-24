0f75ea0cac6b65460568bcbc2d194165daecf0dd
JMarcley <joshua.marcley@gmail.com>
2018-10-24 11:40:57 -0700

    node heroku

---
+++ build/precache-manifest.e463eb3551019cdd3e8a339813bd2e7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9de5c63e32e346a95f0d",
+    "url": "/static/js/main.9de5c63e.chunk.js"
+  },
+  {
+    "revision": "e681bbde13dd676937e2",
+    "url": "/static/js/1.e681bbde.chunk.js"
+  },
+  {
+    "revision": "9de5c63e32e346a95f0d",
+    "url": "/static/css/main.6366e20f.chunk.css"
+  },
+  {
+    "revision": "5cf2618107c41ae1b2b61649d4ee3e34",
+    "url": "/index.html"
+  }
+];
